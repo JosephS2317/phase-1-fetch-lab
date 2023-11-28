73c8c0315528677cf8355ec8f247f826a941b008
Joseph <JosephSped23@gmail.com>
2023-11-28 16:08:26 -0500

    passes all fetch lab tests

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(res => res.json())
+  .then(data => {
+    renderBooks(data)})
+// function fetchBooks() {
+//   return fetch("https://anapioficeandfire.com/api/books")
+//     .then(resp => resp.json())
+//     .then(json => renderBooks(json))
 }
+    //return data;}
+  
+  // To pass the tests, don't forget to return your fetch!
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
+    console.log(book.name)
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
   fetchBooks();
 });
+
+
